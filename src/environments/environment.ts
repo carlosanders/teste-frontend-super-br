f0d7d16dc3223977279dc53ec2367d7153dcd44d
Eduardo Lang <eduardo.lang@agu.gov.br>
2019-07-05 13:03:53 -0300

    ajuste docker para usar o xdebug

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
     jnlp: 'jnlp://127.0.0.1:8000/',
     api_url: 'http://127.0.0.1:8000/v1/',
     mercure_hub: 'http://127.0.0.1:4000/hub',
-    xdebug: '?XDEBUG_SESSION_START=14013'
+    xdebug: '?XDEBUG_SESSION_START=docker'
 };
 
 /*
