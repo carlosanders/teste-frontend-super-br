54c837a513bea86dc429ca218e5a67933e860abf
Eduardo Lang <eduardo.lang@agu.gov.br>
2019-10-23 09:01:22 -0300

    ajustes na modularizacao

--- src/modules/modules-config.ts
+++ src/modules/modules-config.ts
@@ -1,42 +1,5 @@
+import {modulesConfigJudicial} from './judicial/modules-config';
+
 export const modulesConfig = [
-    {
-        name: 'judicial',
-        generoProcesso: ['JUDICIAL'],
-        routes: {
-            'app/main/apps/processo/processo-edit': [
-                {
-                    path: 'dados-basicos-judicial',
-                    // tslint:disable-next-line:max-line-length
-                    loadChildren: () => import(
-                        'modules/judicial/app/main/apps/processo/processo-edit/dados-basicos/dados-basicos-judicial.module'
-                        ).then(m => m.DadosBasicosJudicialModule)
-                }
-            ]
-        },
-        mainMenu: [{
-            id: 'protocolo',
-            entries: [{
-                    id: 'judicial',
-                    title: 'Judicial',
-                    type: 'item',
-                    icon: 'book',
-                    url: '/apps/processo/criar/editar/dados-basicos-judicial'
-                }]
-        }],
-        sidebars: {
-            'app/main/apps/processo/processo-edit/sidebars/main': [
-                {
-                    nome: 'Processo Judicial',
-                    link: 'processo-judicial'
-                }
-            ]
-        },
-        components: {
-            'app/main/apps/tarefas/tarefa-detail': [
-                () => import('modules/judicial/app/main/apps/tarefas/tarefa-detail/button1.module').then(m => {
-                    return {module: m.Button1Module, componentIndex: 0};
-                })
-            ]
-        }
-    }
+    modulesConfigJudicial
 ];
