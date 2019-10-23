cc06a227201bfe572a6b3d5e1e57f9dd71b9115c
Eduardo Lang <eduardo.lang@agu.gov.br>
2019-10-23 08:53:35 -0300

    correcao de bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,32 @@
-import { NgModule, LOCALE_ID } from '@angular/core';
-import { registerLocaleData } from '@angular/common';
+import {NgModule, LOCALE_ID} from '@angular/core';
+import {registerLocaleData} from '@angular/common';
 import localePt from '@angular/common/locales/pt';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ExtraOptions, RouterModule, Routes} from '@angular/router';
-import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import {MatMomentDateModule} from '@angular/material-moment-adapter';
 import {MatButtonModule, MatIconModule, MatSnackBarModule} from '@angular/material';
-import { TranslateModule } from '@ngx-translate/core';
-import { CKEditorModule } from 'ng2-ckeditor';
+import {TranslateModule} from '@ngx-translate/core';
+import {CKEditorModule} from 'ng2-ckeditor';
 import 'hammerjs';
 
-import { FuseModule } from '@fuse/fuse.module';
-import { FuseSharedModule } from '@fuse/shared.module';
-import { FuseProgressBarModule, FuseSidebarModule, FuseThemeOptionsModule } from '@fuse/components';
+import {FuseModule} from '@fuse/fuse.module';
+import {FuseSharedModule} from '@fuse/shared.module';
+import {FuseProgressBarModule, FuseSidebarModule, FuseThemeOptionsModule} from '@fuse/components';
 
-import { fuseConfig } from 'app/fuse-config';
+import {fuseConfig} from 'app/fuse-config';
 
-import { AppComponent } from 'app/app.component';
-import { AppStoreModule } from 'app/store/store.module';
-import { LayoutModule } from 'app/layout/layout.module';
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { AuthGuard } from './main/guard';
-import { LoginInterceptor } from './main/auth/login/login.interceptor';
-import { LogoutInterceptor } from './main/auth/login/logout.interceptor';
-import { LoginService } from './main/auth/login/login.service';
+import {AppComponent} from 'app/app.component';
+import {AppStoreModule} from 'app/store/store.module';
+import {LayoutModule} from 'app/layout/layout.module';
+import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {AuthGuard} from './main/guard';
+import {LoginInterceptor} from './main/auth/login/login.interceptor';
+import {LogoutInterceptor} from './main/auth/login/logout.interceptor';
+import {LoginService} from './main/auth/login/login.service';
 
-import { FakeDbService } from 'app/fake-db/fake-db.service';
+import {FakeDbService} from 'app/fake-db/fake-db.service';
 
 import {ModelModule} from '@cdk/models/model.module';
 
@@ -38,7 +38,7 @@ registerLocaleData(localePt, 'pt');
 
 const appRoutes: Routes = [
     {
-        path        : 'apps',
+        path: 'apps',
         loadChildren: () => import('./main/apps/apps.module').then(m => m.AppsModule),
         canLoad: [AuthGuard]
     },
@@ -60,7 +60,7 @@ const routingConfiguration: ExtraOptions = {
     declarations: [
         AppComponent
     ],
-    imports     : [
+    imports: [
         BrowserModule,
         BrowserAnimationsModule,
         HttpClientModule,
@@ -86,7 +86,7 @@ const routingConfiguration: ExtraOptions = {
         CKEditorModule,
 
         InMemoryWebApiModule.forRoot(FakeDbService, {
-            delay             : 0,
+            delay: 0,
             passThruUnknownUrl: true
         }),
 
@@ -98,17 +98,16 @@ const routingConfiguration: ExtraOptions = {
     ],
     providers: [
         LoginService,
-        { provide: HTTP_INTERCEPTORS, useClass: LoginInterceptor, multi: true },
-        { provide: HTTP_INTERCEPTORS, useClass: LogoutInterceptor, multi: true },
-        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-        { provide: MAT_DATE_LOCALE, useValue: 'pt-BR'},
-        { provide: LOCALE_ID, useValue: 'pt'},
+        {provide: HTTP_INTERCEPTORS, useClass: LoginInterceptor, multi: true},
+        {provide: HTTP_INTERCEPTORS, useClass: LogoutInterceptor, multi: true},
+        {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+        {provide: MAT_DATE_LOCALE, useValue: 'pt-BR'},
+        {provide: LOCALE_ID, useValue: 'pt'},
         AuthGuard
     ],
-    bootstrap   : [
+    bootstrap: [
         AppComponent
     ]
 })
-export class AppModule
-{
+export class AppModule {
 }
