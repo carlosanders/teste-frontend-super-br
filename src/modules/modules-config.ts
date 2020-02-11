82c0e3e50ef8201504d147d42a66757440bf4df9
Jhonatan Rafael Palha Flach <jhonatan.flach@agu.gov.br>
2020-02-11 09:10:00 -0300

    - Removendo instalações de módulos do modules-config.ts

--- src/modules/modules-config.ts
+++ src/modules/modules-config.ts
@@ -1,7 +1,7 @@
-import {modulesConfigJudicial} from './judicial/modules-config';
-import {modulesConfigEcarta} from './ecarta/modules-config';
+//import {modulesConfigJudicial} from './judicial/modules-config';
+//import {modulesConfigEcarta} from './ecarta/modules-config';
 
 export const modulesConfig = [
-    modulesConfigJudicial,
-    modulesConfigEcarta
+//    modulesConfigJudicial,
+//    modulesConfigEcarta
 ];
