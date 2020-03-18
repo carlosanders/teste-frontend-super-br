b79087d65a1e60dd6ec62f55843747792936c992
thiago-amm <thiago-amm@outlook.com>
2020-03-18 11:21:55 -0300

    Estrutura inicial do formulário de cadastro de relevâncias.

---
+++ src/@cdk/components/relevancia/cdk-relevancia-form/cdk-relevancia-form.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'cdk-relevancia-form',
+    templateUrl: './cdk-relevancia-form.component.html',
+    styleUrls: ['./cdk-relevancia-form.component.scss']
+})
+export class CdkRelevanciaFormComponent {
+
+    constructor() {
+
+    }
+
+}
---
+++ src/@cdk/components/relevancia/cdk-relevancia-form/cdk-relevancia-form.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import {CdkRelevanciaFormComponent} from './cdk-relevancia-form.component';
+
+@NgModule({
+    declarations: [
+        CdkRelevanciaFormComponent
+    ],
+    providers: [],
+    exports: [
+        CdkRelevanciaFormComponent
+    ]
+})
+export class CdkRelevanciaFormModule {
+
+}
