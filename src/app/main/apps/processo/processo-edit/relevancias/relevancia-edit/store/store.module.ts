8f309f1dd825f4fccc48ddbad421011927edf494
Wesley Modanez Freitas <wesley-mf@pge.go.gov.br>
2020-03-20 01:18:07 -0300

    Mudança na forma de referenciar os imports.

--- src/app/main/apps/processo/processo-edit/relevancias/relevancia-edit/store/store.module.ts
+++ src/app/main/apps/processo/processo-edit/relevancias/relevancia-edit/store/store.module.ts
@@ -2,8 +2,8 @@ import { StoreModule } from '@ngrx/store';
 import { NgModule } from '@angular/core';
 import { EffectsModule } from '@ngrx/effects';
 
-import {reducers, RelevanciaEditState} from 'app/main/apps/processo/processo-edit/relevancias/relevancia-edit/store/reducers';
-import { effects } from 'app/main/apps/processo/processo-edit/relevancias/relevancia-edit/store/effects';
+import {reducers, RelevanciaEditState} from './reducers';
+import { effects } from './effects';
 
 @NgModule({
     imports  : [
