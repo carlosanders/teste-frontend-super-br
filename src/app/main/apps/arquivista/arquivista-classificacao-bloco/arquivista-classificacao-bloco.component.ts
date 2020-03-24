df6405322c4ed9c4a41bf6b230fc76fd04e3bfe9
jaidson Dantas <jaidson.dantas@agu.gov.br>
2020-03-24 11:28:10 -0300

    [INICIO] Refs #1331, #1652 - Criação basica do componente.

---
+++ src/app/main/apps/arquivista/arquivista-classificacao-bloco/arquivista-classificacao-bloco.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-arquivista-classificacao-bloco',
+  templateUrl: './arquivista-classificacao-bloco.component.html',
+  styleUrls: ['./arquivista-classificacao-bloco.component.scss']
+})
+export class ArquivistaClassificacaoBlocoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/main/apps/arquivista/arquivista-classificacao-bloco/arquivista-classificacao-bloco.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ArquivistaClassificacaoBlocoComponent } from './arquivista-classificacao-bloco.component';
+
+
+
+@NgModule({
+  declarations: [ArquivistaClassificacaoBlocoComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ArquivistaClassificacaoBlocoModule { }
