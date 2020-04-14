9352ae8fe320f2996ea4af03db191dd3d896dca1
Eduardo <eduardo.teodoro@agu.gov.br>
2020-04-14 13:42:32 -0300

    Merge - feature #126

--- src/@cdk/services/processo.service.ts
+++ src/@cdk/services/processo.service.ts
@@ -5,25 +5,18 @@ import {map} from 'rxjs/operators';
 import {Processo, Tarefa} from '@cdk/models';
 import {ModelService} from '@cdk/services/model.service';
 import {plainToClass, classToPlain} from 'class-transformer';
-import {PaginatedResponse} from '@cdk/models';
 import {environment} from 'environments/environment';
 import {Visibilidade} from '@cdk/models';
+import {ParentGenericService} from './parent-generic.service';
 
 @Injectable()
-export class ProcessoService {
+export class ProcessoService extends ParentGenericService<Processo> {
 
     constructor(
-        private modelService: ModelService,
-        private http: HttpClient
+        protected modelService: ModelService,
+        protected http: HttpClient,
     ) {
-    }
-
-    get(id: number, params: HttpParams = new HttpParams(), context: any = '{}'): Observable<Processo> {
-        params['context'] = context;
-        return this.modelService.getOne('processo', id, params)
-            .pipe(
-                map(response => plainToClass(Processo, response)[0])
-            );
+        super(modelService, 'processo', Processo);
     }
 
     downloadAsPdf(id: number | string, sequencial: number | string, params: HttpParams = new HttpParams(), context: any = '{}'): Observable<any> {
@@ -37,7 +30,7 @@ export class ProcessoService {
     }
 
     getVisibilidade(id: number, context: any = '{}'): Observable<any> {
-        const params: HttpParams = new HttpParams()
+        const params: HttpParams = new HttpParams();
         params['context'] = context;
         return this.http.get(`${environment.api_url}${'processo'}/${id}/visibilidade` + environment.xdebug, {params})
             .pipe(
@@ -46,7 +39,7 @@ export class ProcessoService {
     }
 
     createVisibilidade(processoId: number, visibilidade: Visibilidade, context: any = '{}'): Observable<Visibilidade> {
-        const params: HttpParams = new HttpParams()
+        const params: HttpParams = new HttpParams();
         params['context'] = context;
         return this.http.put(
             `${environment.api_url}${'processo'}/${processoId}/${'visibilidade'}` + environment.xdebug,
@@ -58,7 +51,7 @@ export class ProcessoService {
     }
 
     destroyVisibilidade(processoId: number, visibilidadeId: number, context: any = '{}'): Observable<any> {
-        const params: HttpParams = new HttpParams()
+        const params: HttpParams = new HttpParams();
         params['context'] = context;
         return this.http.delete(
             `${environment.api_url}${'processo'}/${processoId}/${'visibilidade'}/${visibilidadeId}` + environment.xdebug,
@@ -66,58 +59,8 @@ export class ProcessoService {
         );
     }
 
-    query(filters: any = '{}', limit: number = 25, offset: number = 0, order: any = '{}', populate: any = '[]', context: any = '{}'): Observable<PaginatedResponse> {
-        const params = {};
-
-        params['where'] = filters;
-        params['limit'] = limit;
-        params['offset'] = offset;
-        params['order'] = order;
-        params['populate'] = populate;
-        params['context'] = context;
-
-        return this.modelService.get('processo', new HttpParams({fromObject: params}))
-            .pipe(
-                map(response => new PaginatedResponse(plainToClass(Processo, response['entities']), response['total']))
-
-            );
-    }
-
-    count(filters: any = '{}', context: any = '{}'): Observable<any> {
-        const params = {};
-        params['where'] = filters;
-        params['context'] = context;
-
-        return this.modelService.count('processo', new HttpParams({fromObject: params}));
-    }
-
-    save(processo: Processo, context: any = '{}'): Observable<Processo> {
-        const params = {};
-        params['context'] = context;
-        if (processo.id) {
-            return this.modelService.put('processo', processo.id, classToPlain(processo), new HttpParams({fromObject: params}))
-                .pipe(
-//                    tap((n) => {console.log('servico PUT' + n); } ),
-                    map(response => {
-                        response = plainToClass(Processo, response);
-                        Object.keys(response).forEach((key) => (response[key] === null) && delete response[key]);
-                        return Object.assign(new Processo(), {...processo, ...response});
-                    })
-                );
-        } else {
-            return this.modelService.post('processo', classToPlain(processo), new HttpParams({fromObject: params}))
-                .pipe(
-                    map(response => {
-                        response = plainToClass(Processo, response);
-                        Object.keys(response).forEach((key) => (response[key] === null) && delete response[key]);
-                        return Object.assign(new Processo(), {...processo, ...response});
-                    })
-                );
-        }
-    }
-
     arquivar(processo: Processo, context: any = '{}'): Observable<Processo> {
-        const params: HttpParams = new HttpParams()
+        const params: HttpParams = new HttpParams();
         params['context'] = context;
         return this.http.patch(
             `${environment.api_url}${'processo'}/${processo.id}/${'arquivar'}` + environment.xdebug,
@@ -132,12 +75,6 @@ export class ProcessoService {
         );
     }
 
-    destroy(id: number, context: any = '{}'): Observable<Processo> {
-        const params = {};
-        params['context'] = context;
-        return this.modelService.delete('processo', id, new HttpParams({fromObject: params}));
-    }
-
     patch(processo: Processo, changes: any): Observable<Processo> {
         return this.http.patch(
             `${environment.api_url}${'processo'}/${processo.id}` + environment.xdebug,
@@ -150,6 +87,4 @@ export class ProcessoService {
             })
         );
     }
-
-
 }
