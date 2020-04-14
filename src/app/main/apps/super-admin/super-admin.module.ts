4f9833d9d0ad86f4077f94f2e2edf98dca43b896
Jaidson de Oliveira Dantas <jaidson.dantas@agu.gov.br>
2020-04-14 11:12:43 -0300

    [CRIAÇÃO] Refs #2340, Criado modulo de SuperAdmin.

--- src/app/main/apps/super-admin/super-admin.module.ts
+++ src/app/main/apps/super-admin/super-admin.module.ts
@@ -1,12 +1,50 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {RouterModule, Routes} from '@angular/router';
 
+import {CdkSidebarModule} from '@cdk/components';
+import {CdkSharedModule} from '@cdk/shared.module';
+import {SuperAdminComponent} from './super-admin.component';
+import { MainSidebarComponent } from './sidebars/main/main-sidebar.component';
+
+const routes: Routes = [
+    {
+        path       : '',
+        component: SuperAdminComponent,
+        children: [
+            // {
+            //     path       : 'tarefas',
+            //     loadChildren: () => import('./setor/setor.module').then(m => m.SetorModule)
+            // },
+            // {
+            //     path       : 'atividades',
+            //     loadChildren: () => import('./usuario/usuario.module').then(m => m.UsuarioModule)
+            // }
+        ],
+        // canActivate: [fromGuards.ResolveGuard]
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+      SuperAdminComponent,
+      MainSidebarComponent
+  ],
+    imports: [
+        CommonModule,
+        CdkSidebarModule,
+        RouterModule.forChild(routes),
+        MatIconModule,
+        RouterModule,
+        CdkSharedModule,
+        MatButtonModule
+    ]
 })
 export class SuperAdminModule { }
