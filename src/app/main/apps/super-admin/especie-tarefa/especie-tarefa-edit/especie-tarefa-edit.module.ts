7a3467f4f077bb1ce80275d56f1c6217f4aa4e85
Jaidson de Oliveira Dantas <jaidson.dantas@agu.gov.br>
2020-04-15 11:26:59 -0300

    [CRIAÇÃO] Refs #2377, #2591 - Criado Store de especie-tarefa Edit

--- src/app/main/apps/super-admin/especie-tarefa/especie-tarefa-edit/especie-tarefa-edit.module.ts
+++ src/app/main/apps/super-admin/especie-tarefa/especie-tarefa-edit/especie-tarefa-edit.module.ts
@@ -5,7 +5,8 @@ import {RouterModule, Routes} from '@angular/router';
 import {
     MatAutocompleteModule,
     MatButtonModule,
-    MatCheckboxModule, MatDatepickerModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
@@ -13,19 +14,25 @@ import {
     MatProgressSpinnerModule,
     MatRippleModule,
     MatSelectModule,
-    MatToolbarModule, MatTooltipModule
+    MatToolbarModule,
+    MatTooltipModule
 } from '../../../../../../@cdk/angular/material';
 import {TranslateModule} from '@ngx-translate/core';
 import {CdkSharedModule} from '../../../../../../@cdk/shared.module';
 import {CdkSidebarModule} from '../../../../../../@cdk/components';
 import {MatStepperModule} from '@angular/material/stepper';
+import * as fromGuards from './store/guards';
+import {ResolveGuard} from './store/guards';
+import {EspecieTarefaEditStoreModule} from './store/store.module';
+import {EspecieTarefaService} from '../../../../../../@cdk/services/especie-tarefa.service';
+import {ColaboradorService} from '../../../../../../@cdk/services/colaborador.service';
 
 
 const routes: Routes = [
     {
         path: ':especieTarefaHandle',
         component: EspecieTarefaEditComponent,
-        // canActivate: [fromGuards.ResolveGuard]
+        canActivate: [fromGuards.ResolveGuard]
     }
 ];
 
@@ -50,11 +57,19 @@ const routes: Routes = [
         MatDatepickerModule,
         MatTooltipModule,
 
+
         TranslateModule,
 
         CdkSharedModule,
         CdkSidebarModule,
         MatStepperModule,
+        EspecieTarefaEditStoreModule
+
+    ],
+    providers: [
+        ResolveGuard,
+        EspecieTarefaService,
+        ColaboradorService
     ]
 })
 export class EspecieTarefaEditModule {
--- src/app/main/apps/super-admin/especie-tarefa/especie-tarefa.module.ts
+++ src/app/main/apps/super-admin/especie-tarefa/especie-tarefa.module.ts
@@ -19,6 +19,7 @@ import {
 import {TranslateModule} from '@ngx-translate/core';
 import {CdkSharedModule} from '../../../../../@cdk/shared.module';
 import {LoginService} from '../../../auth/login/login.service';
+import {EspecieTarefaEditStoreModule} from './especie-tarefa-edit/store/store.module';
 
 const routes: Routes = [
     {
