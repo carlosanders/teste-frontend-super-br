1826c3a64f1c2b6f61537063d3dcde238c624a58
Jaidson de Oliveira Dantas <jaidson.dantas@agu.gov.br>
2020-04-16 11:11:53 -0300

    [CORREÇÕES] Refs #2377, #2590

--- src/app/main/apps/super-admin/especie-tarefa/especie-tarefa.module.ts
+++ src/app/main/apps/super-admin/especie-tarefa/especie-tarefa.module.ts
@@ -19,7 +19,6 @@ import {
 import {TranslateModule} from '@ngx-translate/core';
 import {CdkSharedModule} from '../../../../../@cdk/shared.module';
 import {LoginService} from '../../../auth/login/login.service';
-import {EspecieTarefaEditStoreModule} from './especie-tarefa-edit/store/store.module';
 
 const routes: Routes = [
     {
@@ -46,7 +45,7 @@ const routes: Routes = [
                 path: '**',
                 redirectTo: 'listar'
             }
-        ]
+        ],
     }
 ];
 
