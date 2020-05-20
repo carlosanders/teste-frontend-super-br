d57e3095d118ae4d2406a7b176d916c5e6eec5e2
Eduardo Lang <eduardo.lang@agu.gov.br>
2020-05-20 16:09:30 -0300

    ajustes no ngrx e no logout

--- src/app/main/apps/processo/processo.module.ts
+++ src/app/main/apps/processo/processo.module.ts
@@ -22,6 +22,7 @@ import {VinculacaoEtiquetaService} from '@cdk/services/vinculacao-etiqueta.servi
 import {LoginService} from '../../auth/login/login.service';
 import {ProcessoDownloadModule} from './processo-download/processo-download.module';
 import {MatMenuModule} from '@angular/material/menu';
+import {MatRippleModule} from "@angular/material/core";
 
 const routes: Routes = [
     {
@@ -87,7 +88,8 @@ const routes: Routes = [
         MatTooltipModule,
         CdkVinculacaoEtiquetaChipsModule,
         ProcessoDownloadModule,
-        MatMenuModule
+        MatMenuModule,
+        MatRippleModule
     ],
     providers      : [
         ProcessoService,
