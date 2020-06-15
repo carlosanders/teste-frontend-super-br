9fb3222c6ec4816e800918a7e981568a06f0cd96
Eduardo Lang <eduardo.lang@agu.gov.br>
2020-06-15 18:12:33 -0300

    ajustes de dependencias

--- src/modules/modules-config.ts
+++ src/modules/modules-config.ts
@@ -1,10 +1,10 @@
-import {modulesConfigJudicial} from './judicial/modules-config';
+// import {modulesConfigJudicial} from './judicial/modules-config';
 // import {modulesConfigEcarta} from './ecarta/modules-config';
 // import {modulesConfigDisciplinar} from './disciplinar/modules-config';
 // import {modulesConfigRelatorio} from './relatorio/modules-config';
 
 export const modulesConfig = [
-    modulesConfigJudicial,
+    // modulesConfigJudicial,
     // modulesConfigEcarta,
     // modulesConfigDisciplinar,
     // modulesConfigRelatorio
