cb16124625e746cc7b717fef3df4dd5ee4f54136
Eduardo Lang <eduardo.lang@agu.gov.br>
2020-07-09 16:59:38 -0300

    kubernetes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,9 @@ export const environment = {
     test: false,
     production: true,
     hmr       : false,
-    base_url: 'http://127.0.0.1:8000/',
-    jnlp: 'jnlp://127.0.0.1:8000/',
-    api_url: 'http://127.0.0.1:8000/v1/',
-    mercure_hub: 'http://127.0.0.1:4000/.well-known/mercure',
-    xdebug: '?XDEBUG_SESSION_START=14013'
+    base_url: 'http://suppbackend:8000/',
+    jnlp: 'jnlp://suppbackend:8000/',
+    api_url: 'http://suppbackend:8000/v1/',
+    mercure_hub: 'http://suppbackend:4000/.well-known/mercure',
+    xdebug: ''
 };
