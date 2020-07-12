a4eb814b5a89c1aefee76662ad59649ac863fa94
Eduardo Lang <eduardo.lang@agu.gov.br>
2020-07-12 18:30:58 -0300

    finalizacao do ambiente prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,9 @@ export const environment = {
     test: false,
     production: true,
     hmr       : false,
-    base_url: 'http://suppbackend:8000/',
-    jnlp: 'jnlp://suppbackend:8000/',
-    api_url: 'http://suppbackend:8000/v1/',
-    mercure_hub: 'http://suppbackend:4000/.well-known/mercure',
+    base_url: 'http://suppbackend/',
+    jnlp: 'jnlp://suppbackend/',
+    api_url: 'http://suppbackend/v1/',
+    mercure_hub: 'http://mercure/.well-known/mercure',
     xdebug: ''
 };
