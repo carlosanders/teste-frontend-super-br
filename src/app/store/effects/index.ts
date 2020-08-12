035adc7d292ce41da79a5f11dc62ba5188d46e80
Eduardo Lang <eduardo.lang@agu.gov.br>
2020-08-12 13:15:00 -0300

    correcao de bug

--- src/app/store/effects/index.ts
+++ src/app/store/effects/index.ts
@@ -1,5 +1,10 @@
 import { RouterEffects } from './router.effect';
+import {MercureEffects} from './mercure.effect';
 
-export const effects: any[] = [RouterEffects];
+export const effects: any[] = [
+    RouterEffects,
+    MercureEffects
+];
 
 export * from './router.effect';
+export * from './mercure.effect';
