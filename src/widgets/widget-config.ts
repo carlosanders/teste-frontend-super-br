36fcfb1016caeb6feeefce0a4af1e9391cd48d8d
Eduardo Lang <eduardo.lang@agu.gov.br>
2020-09-03 17:23:35 -0300

    correcao de bug

--- src/widgets/widget-config.ts
+++ src/widgets/widget-config.ts
@@ -4,8 +4,6 @@ import {widgetConfig as tarefasWidgetConfig} from 'app/main/apps/tarefas/widget/
 import {widgetConfig as documentosAvulsosColaboradorWidgetConfig} from 'app/main/apps/documento-avulso/widget/colaborador/widget-config';
 import {widgetConfig as documentosAvulsosConveniadoWidgetConfig} from 'app/main/apps/documento-avulso/widget/conveniado/widget-config';
 import {widgetConfig as tramitacoesWidgetConfig} from 'app/main/apps/processo/widget/widget-config';
-import {widgetConfig as pessoasWidgetConfig} from 'app/main/apps/admin/widget/pessoa/widget-config';
-import {widgetConfig as tarefasCoordenacaoWidgetConfig} from 'app/main/apps/coordenador/widget/tarefas-pendentes/widget-config';
 import {widgetConfig as alertaWidgetConfig} from './widget-alerta/widget-config';
 import {widgetConfig as historicoWidgetConfig} from './widget-historico/widget-config';
 
@@ -14,8 +12,6 @@ export let widgetConfig = [
     ...documentosAvulsosColaboradorWidgetConfig,
     ...documentosAvulsosConveniadoWidgetConfig,
     ...tramitacoesWidgetConfig,
-    ...pessoasWidgetConfig,
-    ...tarefasCoordenacaoWidgetConfig,
     ...alertaWidgetConfig,
     ...historicoWidgetConfig
 ];
