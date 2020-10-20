10aa2dad28685d1464f3c18fdd988fe52babb2d8
edumails <edu.mails@gmail.com>
2020-10-20 16:43:29 -0300

    Correcao de todos os filters e autocompletes - feature #6874

--- src/@cdk/components/volume/sidebars/cdk-volume-filter/cdk-volume-filter.component.ts
+++ src/@cdk/components/volume/sidebars/cdk-volume-filter/cdk-volume-filter.component.ts
@@ -54,9 +54,8 @@ export class CdkVolumeFilterComponent implements OnInit {
     }
 
     // -----------------------------------------------------------------------------------------------------
-    // @ Lifecycle hooks
+    // @ Lifecycle hookss
     // -----------------------------------------------------------------------------------------------------
-
     /**
      * On init
      */
@@ -199,5 +198,5 @@ export class CdkVolumeFilterComponent implements OnInit {
         this.emite();
         this.form.reset();
     }
-    
+
 }
