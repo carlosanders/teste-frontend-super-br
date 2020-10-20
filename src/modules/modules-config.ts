96ff056b5f9fa27cc601f2070df525cb8624a518
edumails <edu.mails@gmail.com>
2020-10-20 16:32:37 -0300

    Correcao de todos os filters e autocompletes - feature #6874

--- src/modules/modules-config.ts
+++ src/modules/modules-config.ts
@@ -1,14 +1,14 @@
-import {modulesConfigJudicial} from './judicial/modules-config';
-import {modulesConfigConsultivo} from './consultivo/modules-config';
-import {modulesConfigDisciplinar} from './disciplinar/modules-config';
-import {modulesConfigDivida} from './divida/modules-config';
+// import {modulesConfigJudicial} from './judicial/modules-config';
+// import {modulesConfigConsultivo} from './consultivo/modules-config';
+// import {modulesConfigDisciplinar} from './disciplinar/modules-config';
+// import {modulesConfigDivida} from './divida/modules-config';
 // import {modulesConfigDivida} from './divida/modules-config';
 
 export const modulesConfig = [
-    modulesConfigJudicial,
-    modulesConfigConsultivo,
-    modulesConfigDisciplinar,
-    modulesConfigDivida
+    // modulesConfigJudicial,
+    // modulesConfigConsultivo,
+    // modulesConfigDisciplinar,
+    // modulesConfigDivida
     // modulesConfigDivida
 ];
 
