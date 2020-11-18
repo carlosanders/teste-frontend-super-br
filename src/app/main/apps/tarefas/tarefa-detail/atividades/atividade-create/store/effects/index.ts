8a73e4e3981aca733ab3e0d277aef386427b9819
Jhonatan Rafael Palha Flach <jhonatan.flach@agu.gov.br>
2020-11-18 15:19:28 -0300

    Implementação de mudança no botão de nova minuta  Correção de bugs relacionados a refresh de tela de documentos mudando o roteamento da aplicação  Correção em store de minutas

--- src/app/main/apps/tarefas/tarefa-detail/atividades/atividade-create/store/effects/index.ts
+++ src/app/main/apps/tarefas/tarefa-detail/atividades/atividade-create/store/effects/index.ts
@@ -1,10 +1,13 @@
 import { AtividadeCreateEffect } from './atividade-create.effects';
 import { AtividadeCreateDocumentosEffect } from './documentos.effects';
+import { ComponentesDigitaisEffects } from './componentes-digitais.effects';
 
 export const effects = [
     AtividadeCreateEffect,
-    AtividadeCreateDocumentosEffect
+    AtividadeCreateDocumentosEffect,
+    ComponentesDigitaisEffects
 ];
 
 export * from './atividade-create.effects';
 export * from './documentos.effects';
+export * from './componentes-digitais.effects';
