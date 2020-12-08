cf1c676882ace3d39a34a85048369c805617e2ca
edumails <edu.mails@gmail.com>
2020-12-08 11:28:37 -0300

    Adicao dos paths - feature #7129

--- src/modules/modules-config.ts
+++ src/modules/modules-config.ts
@@ -1,14 +1,14 @@
 // import {modulesConfigJudicial} from './judicial/modules-config';
 // import {modulesConfigConsultivo} from './consultivo/modules-config';
 // import {modulesConfigDisciplinar} from './disciplinar/modules-config';
-// import {modulesConfigEcarta} from './ecarta/modules-config';
-// import {modulesConfigBarramento} from './barramento/modules-config';
+import {modulesConfigEcarta} from './ecarta/modules-config';
+import {modulesConfigBarramento} from './barramento/modules-config';
 
 export const modulesConfig = [
     // modulesConfigJudicial,
     // modulesConfigConsultivo,
     // modulesConfigDisciplinar
-    // modulesConfigEcarta,
-    // modulesConfigBarramento
+    modulesConfigEcarta,
+    modulesConfigBarramento
 ];
 
