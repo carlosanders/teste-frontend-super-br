f4cdaa0a110ffe517b994a0917e9f541a7c30633
leonardo <leonardo.nobre@agu.gov.br>
2020-12-16 18:59:15 -0300

    ajustes bugs

--- src/modules/modules-config.ts
+++ src/modules/modules-config.ts
@@ -1,16 +1,16 @@
 //import {modulesConfigJudicial} from './judicial/modules-config';
 //import {modulesConfigConsultivo} from './consultivo/modules-config';
 //import {modulesConfigDisciplinar} from './disciplinar/modules-config';
-import {modulesConfigEcarta} from './ecarta/modules-config';
-import {modulesConfigBarramento} from './barramento/modules-config';
+// import {modulesConfigEcarta} from './ecarta/modules-config';
+// import {modulesConfigBarramento} from './barramento/modules-config';
 //import {modulesConfigDivida} from './divida/modules-config';
 
 export const modulesConfig = [
     //modulesConfigJudicial,
     //modulesConfigConsultivo,
     //modulesConfigDisciplinar,
-    modulesConfigEcarta,
-    modulesConfigBarramento,
+    // modulesConfigEcarta,
+    // modulesConfigBarramento,
     //modulesConfigDivida,
 ];
 
