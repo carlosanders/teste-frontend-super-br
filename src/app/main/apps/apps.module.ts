e7a958afa6dde12e13a66ad1e66eab6c6f790908
edumails <edu.mails@gmail.com>
2020-12-22 11:33:17 -0300

    Correcoes de css - feature #7225

--- src/app/main/apps/apps.module.ts
+++ src/app/main/apps/apps.module.ts
@@ -18,30 +18,30 @@ const routes = [
         canActivate: [RoleGuard],
         data: {roles: ['ROLE_ADMIN']}
     },
-    {
-        path        : 'arquivista',
-        loadChildren: () => import('./arquivista/arquivista.module').then(m => m.ArquivistaModule),
-        canActivate: [RoleGuard],
-        data: {roles: ['ROLE_ARQUIVISTA']}
-    },
+    // {
+    //     path        : 'arquivista',
+    //     loadChildren: () => import('./arquivista/arquivista.module').then(m => m.ArquivistaModule),
+    //     canActivate: [RoleGuard],
+    //     data: {roles: ['ROLE_ARQUIVISTA']}
+    // },
     {
         path        : 'coordenador',
         loadChildren: () => import('./coordenador/coordenador.module').then(m => m.CoordenadorModule),
         canActivate: [RoleGuard],
         data: {roles: ['ROLE_COORDENADOR']}
     },
-    {
-        path        : 'tarefas',
-        loadChildren: () => import('./tarefas/tarefas.module').then(m => m.TarefasModule),
-        canActivate: [RoleGuard],
-        data: {roles: ['ROLE_COLABORADOR']}
-    },
-    {
-        path        : 'processo',
-        loadChildren: () => import('./processo/processo.module').then(m => m.ProcessoModule),
-        canActivate: [RoleGuard],
-        data: {roles: ['ROLE_USER']}
-    },
+    // {
+    //     path        : 'tarefas',
+    //     loadChildren: () => import('./tarefas/tarefas.module').then(m => m.TarefasModule),
+    //     canActivate: [RoleGuard],
+    //     data: {roles: ['ROLE_COLABORADOR']}
+    // },
+    // {
+    //     path        : 'processo',
+    //     loadChildren: () => import('./processo/processo.module').then(m => m.ProcessoModule),
+    //     canActivate: [RoleGuard],
+    //     data: {roles: ['ROLE_USER']}
+    // },
     {
         path        : 'documento',
         loadChildren: () => import('./documento/documento.module').then(m => m.DocumentoModule),
@@ -54,42 +54,42 @@ const routes = [
         canActivate: [RoleGuard],
         data: {roles: ['ROLE_COLABORADOR']}
     },
-    {
-        path        : 'pesquisa',
-        loadChildren: () => import('./pesquisa/pesquisa.module').then(m => m.PesquisaModule),
-        canActivate: [RoleGuard],
-        data: {roles: ['ROLE_USER']}
-    },
+    // {
+    //     path        : 'pesquisa',
+    //     loadChildren: () => import('./pesquisa/pesquisa.module').then(m => m.PesquisaModule),
+    //     canActivate: [RoleGuard],
+    //     data: {roles: ['ROLE_USER']}
+    // },
     {
         path        : 'configuracoes',
         loadChildren: () => import('./configuracoes/configuracoes.module').then(m => m.ConfiguracoesModule),
         canActivate: [RoleGuard],
         data: {roles: ['ROLE_USER']}
     },
-    {
-        path        : 'oficios',
-        loadChildren: () => import('./oficios/oficios.module').then(m => m.OficiosModule),
-        canActivate: [RoleGuard],
-        data: {roles: ['ROLE_USER']}
-    },
-    {
-        path        : 'protocolo-externo',
-        loadChildren: () => import('./protocolo-externo/protocolo-externo.module').then(m => m.ProtocoloExternoModule),
-        canActivate: [RoleGuard],
-        data: {roles: ['ROLE_USER']}
-    },
-    {
-        path        : 'calendario',
-        loadChildren: () => import('./calendario/calendar.module').then(m => m.CalendarModule),
-        canActivate: [RoleGuard],
-        data: {roles: ['ROLE_COLABORADOR']}
-    },
-    {
-        path        : 'relatorios',
-        loadChildren: () => import('./relatorios/relatorios.module').then(m => m.RelatoriosModule),
-        canActivate: [RoleGuard],
-        data: {roles: ['ROLE_COLABORADOR']}
-    }
+    // {
+    //     path        : 'oficios',
+    //     loadChildren: () => import('./oficios/oficios.module').then(m => m.OficiosModule),
+    //     canActivate: [RoleGuard],
+    //     data: {roles: ['ROLE_USER']}
+    // },
+    // {
+    //     path        : 'protocolo-externo',
+    //     loadChildren: () => import('./protocolo-externo/protocolo-externo.module').then(m => m.ProtocoloExternoModule),
+    //     canActivate: [RoleGuard],
+    //     data: {roles: ['ROLE_USER']}
+    // },
+    // {
+    //     path        : 'calendario',
+    //     loadChildren: () => import('./calendario/calendar.module').then(m => m.CalendarModule),
+    //     canActivate: [RoleGuard],
+    //     data: {roles: ['ROLE_COLABORADOR']}
+    // },
+    // {
+    //     path        : 'relatorios',
+    //     loadChildren: () => import('./relatorios/relatorios.module').then(m => m.RelatoriosModule),
+    //     canActivate: [RoleGuard],
+    //     data: {roles: ['ROLE_COLABORADOR']}
+    // }
 ];
 
 const path = 'app/main/apps';
