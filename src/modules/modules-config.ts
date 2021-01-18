cc92ba689b6c9ea7cf5f1fa6a22b83d05bd9b35b
Eduardo Lang <eduardo.lang@agu.gov.br>
2021-01-18 17:07:18 -0300

    ajustes

--- src/modules/modules-config.ts
+++ src/modules/modules-config.ts
@@ -1,20 +1,20 @@
-import {modulesConfigJudicial} from './judicial/modules-config';
-import {modulesConfigConsultivo} from './consultivo/modules-config';
-import {modulesConfigDisciplinar} from './disciplinar/modules-config';
+// import {modulesConfigJudicial} from './judicial/modules-config';
+// import {modulesConfigConsultivo} from './consultivo/modules-config';
+// import {modulesConfigDisciplinar} from './disciplinar/modules-config';
 // import {modulesConfigEcarta} from './ecarta/modules-config';
 // import {modulesConfigBarramento} from './barramento/modules-config';
-import {modulesConfigEcarta} from './ecarta/modules-config';
-import {modulesConfigBarramento} from './barramento/modules-config';
-//import {modulesConfigDivida} from './divida/modules-config';
+// import {modulesConfigEcarta} from './ecarta/modules-config';
+// import {modulesConfigBarramento} from './barramento/modules-config';
+// import {modulesConfigDivida} from './divida/modules-config';
 
 export const modulesConfig = [
-    modulesConfigJudicial,
-    modulesConfigConsultivo,
-    modulesConfigDisciplinar,
+    // modulesConfigJudicial,
+    // modulesConfigConsultivo,
+    // modulesConfigDisciplinar,
     // modulesConfigEcarta,
     // modulesConfigBarramento,
-    modulesConfigEcarta,
-    modulesConfigBarramento,
-    //modulesConfigDivida,
+    // modulesConfigEcarta,
+    // modulesConfigBarramento,
+    // modulesConfigDivida,
 ];
 
