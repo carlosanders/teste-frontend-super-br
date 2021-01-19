ce1805895fe5d5fa7955785efa285764abca181b
eduardoromao <abcd1234>
2021-01-19 09:58:50 -0300

    Subindo refactoring do workflow e ajustes necessários em outras funcionalidades.

---
+++ src/app/main/apps/admin/workflow/especies-processo/especies-processo-edit/store/effects/especies-processo-edit.effects.ts
@@ -0,0 +1,123 @@
+import {Injectable} from '@angular/core';
+import {Actions, Effect, ofType} from '@ngrx/effects';
+import {Observable, of} from 'rxjs';
+import {catchError, mergeMap, switchMap, tap} from 'rxjs/operators';
+import * as WorkflowEspecieProcessoEditActions from '../actions/index';
+import * as WorkflowEspecieProcessoListActions  from '../../../especies-processo-list/store/actions/index';
+import {AddData, SetData} from '@cdk/ngrx-normalizr';
+import {especieProcesso as especieProcessoSchema, workflow as workflowSchema} from '@cdk/normalizr';
+import {Router} from '@angular/router';
+import {select, Store} from '@ngrx/store';
+import {getRouterState, State} from 'app/store/reducers';
+import {EspecieProcessoService} from '@cdk/services/especie-processo.service';
+import {EspecieProcesso} from '@cdk/models';
+
+@Injectable()
+export class EspeciesProcessoEditEffects {
+    routerState: any;
+
+    constructor(
+        private _actions: Actions,
+        private _especieProcessoService: EspecieProcessoService,
+        private _store: Store<State>,
+        private _router: Router
+    ) {
+        this._store
+            .pipe(select(getRouterState))
+            .subscribe(routerState => {
+                if (routerState) {
+                    this.routerState = routerState.state;
+                }
+            });
+    }
+
+    /**
+     * Get EspecieProcesso with router parameters
+     * @type {Observable<any>}
+     */
+    @Effect()
+    getEspecieProcesso: any =
+        this._actions
+            .pipe(
+                ofType<WorkflowEspecieProcessoEditActions.GetEspecieProcesso>(WorkflowEspecieProcessoEditActions.GET_ESPECIE_PROCESSO),
+                switchMap((action) => {
+                    return this._especieProcessoService.query(
+                        JSON.stringify(action.payload),
+                        1,
+                        0,
+                        JSON.stringify({}),
+                        JSON.stringify([
+                            'populateAll'
+                        ]),
+                        JSON.stringify({isAdmin: true}));
+                }),
+                switchMap(response => [
+                    new AddData<EspecieProcesso>({data: response['entities'], schema: especieProcessoSchema}),
+                    new WorkflowEspecieProcessoEditActions.GetEspecieProcessoSuccess({
+                        loaded: {
+                            id: 'especieProcessoHandle',
+                            value: this.routerState.params.especieProcessoHandle
+                        },
+                        entityId: response['entities'][0].id
+                    })
+                ]),
+                catchError((err, caught) => {
+                    this._store.dispatch(new WorkflowEspecieProcessoEditActions.GetEspecieProcessoFailed(err));
+                    return caught;
+                })
+            );
+
+    /**
+     * Update EspecieProcesso
+     * @type {Observable<any>}
+     */
+    @Effect()
+    updateEspecieProcesso: any =
+        this._actions
+            .pipe(
+                ofType<WorkflowEspecieProcessoEditActions.UpdateEspecieProcesso>(WorkflowEspecieProcessoEditActions.UPDATE_ESPECIE_PROCESSO),
+                switchMap((action) => {
+                    return this._especieProcessoService.patch(action.payload.especieProcesso, action.payload.changes).pipe(
+                        mergeMap((response: EspecieProcesso) => [
+                            new SetData<EspecieProcesso>({data: [response], schema: especieProcessoSchema}),
+                            new WorkflowEspecieProcessoListActions.GetEspecieProcesso(
+                                {
+                                    filter: {
+                                        'workflow.id': 'eq:' + this.routerState.params.workflowHandle
+                                    },
+                                    gridFilter: {},
+                                    limit: 10,
+                                    offset: 0,
+                                    sort: {criadoEm: 'ASC'},
+                                    populate: [
+                                        'populateAll'
+                                    ],
+                                    context: {isAdmin: true},
+                                    deletingIds: [],
+                                    deletedIds: []
+                                }
+                            ),
+                            new WorkflowEspecieProcessoEditActions.UpdateEspecieProcessoSuccess(response)
+                        ])
+                    );
+                }),
+                catchError((err, caught) => {
+                    this._store.dispatch(new WorkflowEspecieProcessoEditActions.UpdateEspecieProcessoFailed(err));
+                    return caught;
+                })
+            );
+
+    /**
+     * Save EspecieProcesso Success
+     */
+    @Effect({dispatch: false})
+    updateEspecieProcessoSuccess: any =
+        this._actions
+            .pipe(
+                ofType<WorkflowEspecieProcessoEditActions.UpdateEspecieProcessoSuccess>(WorkflowEspecieProcessoEditActions.UPDATE_ESPECIE_PROCESSO_SUCCESS),
+                tap((action) => {
+                    this._router.navigate([this._router.url.replace('editar/criar', 'listar')]).then();
+                })
+            );
+
+}
---
+++ src/app/main/apps/admin/workflow/especies-processo/especies-processo-list/store/selectors/especies-processo-list.selectors.ts
@@ -0,0 +1,53 @@
+import {createSelector} from '@ngrx/store';
+import {
+    getEspecieProcessoListAppState,
+    WorkflowEspecieProcessoListAppState,
+    WorkflowEspecieProcessoListState
+} from '../reducers';
+
+import {createSchemaSelectors} from '@cdk/ngrx-normalizr';
+import {especieProcesso as especieProcessoSchema} from '@cdk/normalizr';
+import {EspecieProcesso} from '@cdk/models';
+
+const schemaSelectors = createSchemaSelectors<EspecieProcesso>(especieProcessoSchema);
+
+export const getEspecieProcessoListState = createSelector(
+    getEspecieProcessoListAppState,
+    (state: WorkflowEspecieProcessoListAppState) => state.especieProcessoList
+);
+
+export const getEspecieProcessoListIds = createSelector(
+    getEspecieProcessoListState,
+    (state: WorkflowEspecieProcessoListState) => state.entitiesId
+);
+
+export const getEspecieProcessoList = createSelector(
+    schemaSelectors.getNormalizedEntities,
+    getEspecieProcessoListIds,
+    schemaSelectors.entitiesProjector
+);
+
+export const getPagination = createSelector(
+    getEspecieProcessoListState,
+    (state: WorkflowEspecieProcessoListState) => state.pagination
+);
+
+export const getEspecieProcessoListLoaded = createSelector(
+    getEspecieProcessoListState,
+    (state: WorkflowEspecieProcessoListState) => state.loaded
+);
+
+export const getIsLoading = createSelector(
+    getEspecieProcessoListState,
+    (state: WorkflowEspecieProcessoListState) => state.loading
+);
+
+export const getDeletingIds = createSelector(
+    getEspecieProcessoListState,
+    (state: WorkflowEspecieProcessoListState) => state.deletingIds
+);
+
+export const getDeletedIds = createSelector(
+    getEspecieProcessoListState,
+    (state: WorkflowEspecieProcessoListState) => state.deletedIds
+);
