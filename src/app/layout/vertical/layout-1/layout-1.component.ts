0a29c3d51f34eb0fe6dd5071a3dcdb162c82ff15
Jhonatan Rafael Palha Flach <jhonatan.flach@agu.gov.br>
2021-01-25 15:41:07 -0300

    Replicação de mudanças no quick-panel pros 3 layouts do sistema além do vertical-1  #7465

--- src/app/layout/vertical/layout-1/layout-1.component.ts
+++ src/app/layout/vertical/layout-1/layout-1.component.ts
@@ -25,13 +25,9 @@ export class VerticalLayout1Component implements OnInit, OnDestroy
     @HostListener('window:resize', ['$event'])
     onResize(event) {
         this.innerWidth = window.innerWidth;
-        if(innerWidth<=600) {
-            this.mobileMode = true;
-        }
-        else {
-            this.mobileMode = false;
-        }
+        this.mobileMode = innerWidth <= 600;
     }
+
     /**
      * Constructor
      *
@@ -65,12 +61,7 @@ export class VerticalLayout1Component implements OnInit, OnDestroy
             });
 
         this.innerWidth = window.innerWidth;
-        if(innerWidth<=600) {
-            this.mobileMode = true;
-        }
-        else {
-            this.mobileMode = false;
-        }
+        this.mobileMode = innerWidth <= 600;
     }
 
     /**
