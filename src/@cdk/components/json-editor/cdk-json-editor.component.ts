09748908b17f2cf91590689785a228d1a73398e1
Jonas Barbosa <jbsilva023@gmail.com>
2021-03-01 12:51:07 -0300

    [ADD] cdk-json-editor

---
+++ src/@cdk/components/json-editor/cdk-json-editor.component.ts
@@ -0,0 +1,46 @@
+import {ChangeDetectionStrategy, Component, Input, ViewChild, ViewEncapsulation} from '@angular/core';
+import { JsonEditorComponent, JsonEditorOptions } from 'ang-jsoneditor';
+import {cdkAnimations} from '@cdk/animations';
+
+@Component({
+    selector: 'cdk-json-editor',
+    templateUrl: './cdk-json-editor.component.html',
+    styleUrls: ['./cdk-json-editor.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    encapsulation: ViewEncapsulation.None,
+    animations: cdkAnimations,
+})
+export class CdkJsonEditorComponent {
+
+    @Input()
+    editorOptions: JsonEditorOptions;
+
+    @Input()
+    dataSchema: any;
+
+    @ViewChild(JsonEditorComponent, { static: false }) editor: JsonEditorComponent;
+
+    constructor() {
+        this.editorOptions = new JsonEditorOptions()
+        this.editorOptions.modes = ['code', 'text', 'tree', 'view']; // set all allowed modes
+        //this.options.mode = 'code'; //set only one mode
+
+        this.dataSchema = {
+            "products":[
+                {"name":"car",
+                    "product":[
+                        {"name":"honda",
+                            "model":[
+                                {"id":"civic","name":"civic"},
+                                {"id":"accord","name":"accord"},
+                                {"id":"crv","name":"crv"},
+                                {"id":"pilot","name":"pilot"},
+                                {"id":"odyssey","name":"odyssey"}
+                            ]
+                        }
+                    ]
+                }
+            ]
+        };
+    }
+}
