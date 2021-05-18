c3c1c3fe83ed24443808dcffef3a0f7e16072343
Eduardo Lang <eduardo.lang@agu.gov.br>
2021-05-18 09:49:22 -0300

    correcao

--- src/app/main/apps/documento/documento-edit/assinaturas/store/reducers/assinaturas.reducer.ts
+++ src/app/main/apps/documento/documento-edit/assinaturas/store/reducers/assinaturas.reducer.ts
@@ -100,7 +100,7 @@ export function AssinaturasReducer(
                 ...state,
                 deletingIds: state.deletingIds.filter(id => id !== action.payload),
                 deletedIds: [...state.deletedIds, action.payload],
-                deletingErrors: _.omit(this.state.deletingErrors, [action.payload])
+                deletingErrors: _.omit(state.deletingErrors, [action.payload])
             };
         }
 
