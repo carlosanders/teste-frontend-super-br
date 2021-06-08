1c652fb5d48d700e3c20097a449581ecd2296b03
eduardoromao <abcd1234>
2021-06-08 18:04:35 -0300

    [ISSUE-197] Corrigindo tempo de exibição da notificação.

--- src/notification/download-processo/notification-config.ts
+++ src/notification/download-processo/notification-config.ts
@@ -4,7 +4,7 @@ const notification = new Notification();
 notification.module = () => import('notification/download-processo/download-processo.module').then(m => m.DownloadProcessoModule);
 notification.order = 1;
 notification.config = {
-    duration: 500000,
+    duration: 5000,
     horizontalPosition: 'center',
     verticalPosition: 'top',
     panelClass: ['notificacao-snackbar']
