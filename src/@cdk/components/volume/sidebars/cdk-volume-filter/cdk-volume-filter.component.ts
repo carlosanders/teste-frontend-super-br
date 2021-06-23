76372e19c9445f7aa3d498f047629cadda6f112d
Eduardo Lang <eduardo.lang@agu.gov.br>
2021-06-23 09:26:42 -0300

    ajuste

--- src/@cdk/components/volume/sidebars/cdk-volume-filter/cdk-volume-filter.component.ts
+++ src/@cdk/components/volume/sidebars/cdk-volume-filter/cdk-volume-filter.component.ts
@@ -62,12 +62,16 @@ export class CdkVolumeFilterComponent {
             andXFilter.push({'origemDados.id': `eq:${this.form.get('origemDados').value.id}`});
         }
 
-        if (this.filterCriadoEm.length > 0) {
-            andXFilter.push(this.filterCriadoEm[0]);
+        if (this.filterCriadoEm?.length) {
+            this.filterCriadoEm.forEach((filter) => {
+                andXFilter.push(filter);
+            });
         }
 
-        if (this.filterAtualizadoEm.length > 0) {
-            andXFilter.push(this.filterAtualizadoEm[0]);
+        if (this.filterCriadoEm?.length) {
+            this.filterCriadoEm.forEach((filter) => {
+                andXFilter.push(filter);
+            });
         }
 
         if (this.form.get('criadoPor').value) {
