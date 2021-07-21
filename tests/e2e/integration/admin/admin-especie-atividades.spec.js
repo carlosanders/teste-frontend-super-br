d376876ed6848c45ae1dd0fc6eb94155018cbb3c
Victor Hufo <victor.nunes@datainfo.inf.br>
2021-07-21 17:04:56 -0300

    [TEST] Testes #191

---
+++ tests/e2e/integration/admin/admin-especie-atividades.spec.js
@@ -0,0 +1,22 @@
+context('Administrador', () => {
+    beforeEach(() => {
+        cy.login();
+    })
+    it('Administrador -> Especie de Atividades', () => {
+        cy.get('[fxflex="1 0 auto"] > .mat-focus-indicator > .mat-button-wrapper > .mat-icon').click();
+        cy.get('.nav-link:contains("Administrador")').scrollIntoView();
+        cy.get('.nav-link:contains("Administrador")').click();
+        cy.get('.mat-ripple:contains("Classificacoes")').click();
+        cy.get('.back > .mat-focus-indicator > .mat-button-wrapper > .mat-icon:contains("add")', {timeout: 40000}).click();
+        cy.wait(3000);
+        cy.get('.mat-input-element[formControlName="nome"]').type("teste");
+        cy.get('.mat-input-element[formControlName="descricao"]').type("teste");
+        cy.get('.mat-input-element[formControlName="generoAtividade"]').type("ADMINSTRATIVO");
+        cy.get('.mat-checkbox[formControlName="ATIVO"]').click();
+        cy.get('.mat-button-wrapper:contains("SALVAR")').click();
+        cy.get('.mat-row > .cdk-column-nome:contains("TESTE")').first().parent().find('.mat-icon-no-color:contains("delete")').click();
+        cy.get('.mat-row > .cdk-column-nome:contains("TESTE")').first().should('have.class','deleted');
+
+
+    })
+});
