080df1741937a8089e58ef88bb0b5bb769fec2c2
leonardo <leonardo.nobre@agu.gov.br>
2021-09-06 16:15:56 -0300

    ajuste issue 617

--- src/app/main/apps/documento/documento-edit/sigilos/documento-edit-sigilos.component.ts
+++ src/app/main/apps/documento/documento-edit/sigilos/documento-edit-sigilos.component.ts
@@ -238,9 +238,7 @@ export class DocumentoEditSigilosComponent implements OnInit, OnDestroy, AfterVi
             sort: params.sort,
             limit: params.limit,
             offset: params.offset,
-            populate: [
-                ...this.pagination.populate
-            ]
+            populate: []
         }));
     }
 }
