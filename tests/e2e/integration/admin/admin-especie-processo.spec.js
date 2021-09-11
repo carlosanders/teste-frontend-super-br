71b6b7ac1eda3a5511a49fd08cb3e0b3d2b1b2e2
Higo Cavalcante <higodf@gmail.com>
2021-09-10 22:54:54 -0300

    feat: Ajustes nos testes automatizados do menu processo e administração
    Refs #628

--- tests/e2e/integration/admin/admin-especie-processo.spec.js
+++ tests/e2e/integration/admin/admin-especie-processo.spec.js
@@ -1,24 +1,72 @@
-context('Administrador', () => {
-    beforeEach(() => {
-        cy.login();
+/// <reference types="cypress" />
+
+import Administracao from '../pageObjects/administracao'
+
+describe('Teste para cadastrar, editar e exluir espécies de processo', function () {
+
+    let paramNome
+
+  beforeEach(function () {
+    cy.fixture('admin/admin-especie-processo.json').then((fixture) => {
+        this.admin = fixture.admin;
     })
+  })
 
-    //Teste desativado por erro de implementação, deverá ser reativado assim que for corrigido
-    it.skip('Administrador -> Especie de Processos', () => {
-        cy.get('[fxflex="1 0 auto"] > .mat-focus-indicator > .mat-button-wrapper > .mat-icon').click();
-        cy.get('.nav-link:contains("Administrador")').scrollIntoView();
-        cy.get('.nav-link:contains("Administrador")').click();
-        cy.get('.mat-ripple:contains("Classificacoes")').click();
-        cy.get('.back > .mat-focus-indicator > .mat-button-wrapper > .mat-icon:contains("add")', {timeout: 40000}).click();
-        cy.wait(3000);
-        cy.get('.mat-input-element[formControlName="nome"]').type("teste");
-        cy.get('.mat-input-element[formControlName="descricao"]').type("teste");
-        cy.get('.mat-input-element[formControlName="classificacao"]').type("ADMINSTRATIVO");
-        cy.get('.mat-checkbox[formControlName="ATIVO"]').click();
-        cy.get('.mat-button-wrapper:contains("SALVAR")').click();
-        cy.get('.mat-row > .cdk-column-nome:contains("TESTE")').first().parent().find('.mat-icon-no-color:contains("delete")').click();
-        cy.get('.mat-row > .cdk-column-nome:contains("TESTE")').first().should('have.class','deleted');
+  it('Deve permitir cadastrar, editar e exluir espécies de processo', function () {
 
+    //locar no sistema
+    cy.login('00000000004') 
+    cy.visit("./apps/admin/especie-processo/listar")
+    const administracao = new Administracao()
 
+    paramNome = this.admin.nome + Math.random()
+
+    administracao.getAdicionar().click()
+    cy.wait(1000)
+    administracao.getNome().clear().type(paramNome)
+    administracao.getDescricaoTextArea().clear().type(this.admin.descricao)
+    administracao.completeGeneroProcesso(this.admin.generoProcesso)
+    this.admin.classificacao.map(function(data){
+        administracao.completeClassificacao(data.nome1)
     })
-});
+    this.admin.modalidadeMeio.map(function(data){
+        administracao.completeModalidadeMeio(data.valor1)
+    })
+    administracao.getTitulo().clear().type(this.admin.titulo)
+    administracao.getSituacao().click()
+    administracao.salvar().click()
+
+    // Pesquisar item cadastrado
+    administracao.getFiltrar().click()
+    administracao.getNome().clear().type(paramNome)
+    administracao.buscar().click()    
+    administracao.getTabelaAviso().should('be.visible')
+    administracao.getTabelaAviso().should("contain.text", paramNome)
+
+    // Editar registro
+    administracao.getItemEditar(paramNome, "edit").should('be.visible')
+    administracao.getItemEditar(paramNome, "edit").click()
+    cy.wait(1000)
+    this.admin.classificacao.map(function(data){
+        administracao.completeClassificacao(data.nome2)
+    })
+    this.admin.modalidadeMeio.map(function(data){
+        administracao.completeModalidadeMeio(data.valor2)
+    })
+    administracao.salvar().click()
+
+    // Remover registro
+    // Pesquisar registro inativo
+    administracao.getFiltrar().click()
+    administracao.getNome().clear().type(paramNome)
+    administracao.buscar().click()
+    cy.wait(1000)
+    administracao.btnInativos().should('be.visible')
+    administracao.btnInativos().should('be.enabled')
+    administracao.btnInativos().click()
+    administracao.getTabelaAviso().should('be.visible')
+    administracao.getTabelaAviso().should("contain.text", paramNome)
+
+  })
+
+})
