5330ec7f6081d06992da5a8638f0291db84d1527
Icaro Melo <icaro.melo@agu.gov.br>
2021-09-16 14:14:15 -0300

    Revertendo unload operacoes

--- src/app/store/actions/operacoes.actions.ts
+++ src/app/store/actions/operacoes.actions.ts
@@ -2,7 +2,6 @@ import {Action} from '@ngrx/store';
 
 export const RESULTADO = '[OPERACOES] RESULTADO';
 export const OPERACAO = '[OPERACOES] OPERACAO';
-export const UNLOAD_OPERACAO = '[OPERACOES] UNLOAD OPERACAO';
 export const SET_CURRENT_LOTE = '[OPERACOES] SET CURRENT LOTE';
 
 /**
@@ -31,15 +30,6 @@ export class Operacao implements Action
     }
 }
 
-export class UnloadOperacao implements Action
-{
-    readonly type = UNLOAD_OPERACAO;
-
-    constructor()
-    {
-    }
-}
-
 export class SetCurrentLote implements Action
 {
     readonly type = SET_CURRENT_LOTE;
@@ -52,5 +42,4 @@ export class SetCurrentLote implements Action
 export type OperacoesActionsAll
     = Resultado
     | Operacao
-    | UnloadOperacao
     | SetCurrentLote;
