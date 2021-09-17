0f0f0cf7855ca52815978617a4634771e383b90c
Icaro Melo <icaro.melo@agu.gov.br>
2021-09-17 14:40:09 -0300

    Correção retirada das menções sapiens, supp e agu na ajuda do sistema

--- src/app/main/apps/tarefas/tarefa-detail/atividades/atividade-create/ajuda/topicos-config.ts
+++ src/app/main/apps/tarefas/tarefa-detail/atividades/atividade-create/ajuda/topicos-config.ts
@@ -2,7 +2,7 @@ import {Topico} from 'ajuda/topico';
 
 const topico = new Topico();
 topico.titulo = 'Lançando uma atividade';
-topico.descricao = 'Como lançar uma atividade no SUPP';
+topico.descricao = 'Como lançar uma atividade';
 topico.module = () => import('app/main/apps/tarefas/tarefa-detail/atividades/atividade-create/ajuda/ajuda-atividade-create.module').then(m => m.AjudaAtividadeCreateModule);
 
 export const topicosConfig =
