b1f79f52ec6f56187a2990b3d69853ed985b5cb7
Icaro Melo <icaro.melo@agu.gov.br>
2021-09-29 15:50:15 -0300

    Melhoria implementação do botão de adição de Coordenadores para Coordenador de Órgão Central e Coordenador de Unidade

---
+++ src/app/main/apps/coordenador/usuarios/coordenadores/coordenadores-list/store/selectors/coordenadores-list.selectors.ts
@@ -0,0 +1,60 @@
+import {createSelector} from '@ngrx/store';
+import {CoordenadoresListAppState, CoordenadoresListState, getCoordenadoresListAppState} from '../reducers';
+
+import {createSchemaSelectors} from '@cdk/ngrx-normalizr';
+import {coordenador as coordenadoreschema} from '@cdk/normalizr';
+import {Coordenador} from '@cdk/models/coordenador.model';
+import {getProcessoState, ProcessoState} from "../../../../../../processo/store";
+
+const schemaSelectors = createSchemaSelectors<Coordenador>(coordenadoreschema);
+
+export const getCoordenadoresListState = createSelector(
+    getCoordenadoresListAppState,
+    (state: CoordenadoresListAppState) => state.coordenadoresList
+);
+
+export const getCoordenadoresListIds = createSelector(
+    getCoordenadoresListState,
+    (state: CoordenadoresListState) => state.entitiesId
+);
+
+export const getCoordenadoresList = createSelector(
+    schemaSelectors.getNormalizedEntities,
+    getCoordenadoresListIds,
+    schemaSelectors.entitiesProjector
+);
+
+export const getPagination = createSelector(
+    getCoordenadoresListState,
+    (state: CoordenadoresListState) => state.pagination
+);
+
+export const getCoordenadoresListLoaded = createSelector(
+    getCoordenadoresListState,
+    (state: CoordenadoresListState) => state.loaded
+);
+
+export const getIsLoading = createSelector(
+    getCoordenadoresListState,
+    (state: CoordenadoresListState) => state.loading
+);
+
+export const getDeletingIds = createSelector(
+    getCoordenadoresListState,
+    (state: CoordenadoresListState) => state.deletingIds
+);
+
+export const getDeletedIds = createSelector(
+    getCoordenadoresListState,
+    (state: CoordenadoresListState) => state.deletedIds
+);
+
+export const getDeletingErrors = createSelector(
+    getCoordenadoresListState,
+    (state: CoordenadoresListState) => state.deletingErrors
+);
+
+export const getErrors = createSelector(
+    getCoordenadoresListState,
+    (state: CoordenadoresListState) => state.errors
+);
