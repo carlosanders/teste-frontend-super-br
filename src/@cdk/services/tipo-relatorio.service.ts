07832d95bdee90b9aee1b0b185b8768d7921e502
Leonardo <leonardo.nobre@agu.gov.br>
2021-10-07 11:29:03 -0300

    tela de restricao de acesso para tipos de relatorios

--- src/@cdk/services/tipo-relatorio.service.ts
+++ src/@cdk/services/tipo-relatorio.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {ParentGenericService} from './parent-generic.service';
 import {ModelService} from '@cdk/services/model.service';
 import {TipoRelatorio} from '@cdk/models/tipo-relatorio.model';
@@ -7,6 +7,7 @@ import {Observable} from 'rxjs';
 import {environment} from '../../environments/environment';
 import {map} from 'rxjs/operators';
 import {plainToClass} from 'class-transformer';
+import {Visibilidade} from '../models';
 
 @Injectable()
 export class TipoRelatorioService extends ParentGenericService<TipoRelatorio> {
@@ -20,7 +21,7 @@ export class TipoRelatorioService extends ParentGenericService<TipoRelatorio> {
 
     patch(tipoRelatorio: TipoRelatorio, changes: any): Observable<TipoRelatorio> {
         return this.http.patch(
-            `${environment.api_url}${'administrativo/tipoRelatorio'}/${tipoRelatorio.id}` + environment.xdebug,
+            `${environment.api_url}${'administrativo/tipo_relatorio'}/${tipoRelatorio.id}` + environment.xdebug,
             JSON.stringify(changes)
         ).pipe(
             map((response) => {
@@ -28,6 +29,37 @@ export class TipoRelatorioService extends ParentGenericService<TipoRelatorio> {
                 Object.keys(response).forEach(key => (response[key] === null) && delete response[key]);
                 return Object.assign(new TipoRelatorio(), {...tipoRelatorio, ...response});
             })
+        );
+    }
+
+
+    getVisibilidade(id: number, context: any = '{}'): Observable<any> {
+        const params: HttpParams = new HttpParams();
+        params['context'] = context;
+        return this.http.get(`${environment.api_url}administrativo/${'tipo_relatorio'}/${id}/visibilidade` + environment.xdebug, {params})
+            .pipe(
+                map(response => plainToClass(Visibilidade, response))
+            );
+    }
+
+    createVisibilidade(tipoRelatorioId: number, visibilidade: Visibilidade, context: any = '{}'): Observable<Visibilidade> {
+        const params: HttpParams = new HttpParams();
+        params['context'] = context;
+        return this.http.put(
+            `${environment.api_url}${'administrativo/tipo_relatorio'}/${tipoRelatorioId}/${'visibilidade'}` + environment.xdebug,
+            JSON.stringify(visibilidade),
+            {params}
+        ).pipe(
+            map(response => plainToClass(Visibilidade, response))
+        );
+    }
+
+    destroyVisibilidade(tipoRelatorioId: number, visibilidadeId: number, context: any = '{}'): Observable<any> {
+        const params: HttpParams = new HttpParams();
+        params['context'] = context;
+        return this.http.delete(
+            `${environment.api_url}${'administrativo/tipo_relatorio'}/${tipoRelatorioId}/${'visibilidade'}/${visibilidadeId}` + environment.xdebug,
+            {params}
         );
     }
 }
