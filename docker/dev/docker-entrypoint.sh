78b68b786da6e407fe575aa3c2570e666550a070
Eduardo Romão <eduardo.romao@outlook.com>
2022-05-11 10:19:54 -0300

    fix: rollback
    Removendo alteração enviada por engano.
    
    Ref: #238

--- docker/dev/docker-entrypoint.sh
+++ docker/dev/docker-entrypoint.sh
@@ -5,9 +5,9 @@ export NG_CLI_ANALYTICS=ci
 export NODE_OPTIONS=--max_old_space_size=12192
 export CYPRESS_CACHE_FOLDER=./tmp/Cypress
 
-#rm -rf /app/package-lock.json
-#
-#npm install --legacy-peer-deps --package-lock=false
+rm -rf /app/package-lock.json
+
+npm install --legacy-peer-deps --package-lock=false
 
 node node_modules/@angular/cli/bin/ng serve --host 0.0.0.0 --port 4200
 
