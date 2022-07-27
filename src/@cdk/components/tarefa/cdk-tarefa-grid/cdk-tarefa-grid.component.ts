5e4e2fcf635c3fb941c8589974a42d11cc7a28a3
Marcelo Santos <marcelo.santos@ebserh.gov.br>
2022-07-27 18:30:49 -0300

    Inclusão da função de ordenação

--- src/@cdk/components/tarefa/cdk-tarefa-grid/cdk-tarefa-grid.component.ts
+++ src/@cdk/components/tarefa/cdk-tarefa-grid/cdk-tarefa-grid.component.ts
@@ -7,7 +7,7 @@ import {
     Input,
     OnChanges,
     OnInit,
-    Output,
+    Output, SimpleChanges,
     ViewChild,
     ViewEncapsulation
 } from '@angular/core';
@@ -16,10 +16,14 @@ import {merge, of} from 'rxjs';
 import {cdkAnimations} from '@cdk/animations';
 import {CdkSidebarService} from '@cdk/components/sidebar/sidebar.service';
 import {MatPaginator, MatSort} from '@cdk/angular/material';
-import {debounceTime, distinctUntilChanged, switchMap, tap} from 'rxjs/operators';
+import {tap} from 'rxjs/operators';
 import {TarefaDataSource} from '@cdk/data-sources/tarefa-data-source';
 import {Tarefa} from '@cdk/models';
-import {FormControl} from '@angular/forms';
+import {TableColumn} from "../../table-definitions/table-column";
+import {TableDefinitionsService} from "../../table-definitions/table-definitions.service";
+import {CdkTarefaGridColumns} from "./cdk-tarefa-grid.columns";
+import {CdkTableGridComponent} from "../../table-definitions/cdk-table-grid.component";
+import * as _ from "lodash";
 
 @Component({
     selector: 'cdk-tarefa-grid',
@@ -29,7 +33,7 @@ import {FormControl} from '@angular/forms';
     encapsulation: ViewEncapsulation.None,
     animations: cdkAnimations
 })
-export class CdkTarefaGridComponent implements AfterViewInit, OnInit, OnChanges {
+export class CdkTarefaGridComponent extends CdkTableGridComponent implements AfterViewInit, OnInit, OnChanges {
 
     @Input()
     loading = false;
@@ -49,131 +53,6 @@ export class CdkTarefaGridComponent implements AfterViewInit, OnInit, OnChanges 
     @Input()
     displayedColumns: string[] = ['select', 'id', 'processo.nup', 'especieTarefa.nome', 'dataHoraInicioPrazo',
         'dataHoraFinalPrazo', 'actions'];
-
-    allColumns: any[] = [
-        {
-            id: 'select',
-            label: '',
-            fixed: true
-        },
-        {
-            id: 'id',
-            label: 'Id',
-            fixed: true
-        },
-        {
-            id: 'processo.nup',
-            label: 'NUP',
-            fixed: true
-        },
-        {
-            id: 'urgente',
-            label: 'Urgente',
-            fixed: false
-        },
-        {
-            id: 'observacao',
-            label: 'Observação',
-            fixed: false
-        },
-        {
-            id: 'redistribuida',
-            label: 'Redistribuída',
-            fixed: false
-        },
-        {
-            id: 'dataHoraLeitura',
-            label: 'Data da Leitura',
-            fixed: false
-        },
-        {
-            id: 'dataHoraDistribuicao',
-            label: 'Data da Distribuição',
-            fixed: false
-        },
-        {
-            id: 'dataHoraInicioPrazo',
-            label: 'Data do Início do Prazo',
-            fixed: false
-        },
-        {
-            id: 'dataHoraFinalPrazo',
-            label: 'Data do Final do Prazo',
-            fixed: false
-        },
-        {
-            id: 'dataHoraConclusaoPrazo',
-            label: 'Data da Conclusão do Prazo',
-            fixed: false
-        },
-        {
-            id: 'especieTarefa.nome',
-            label: 'Espécie de Tarefa',
-            fixed: false
-        },
-        {
-            id: 'usuarioResponsavel.nome',
-            label: 'Usuário Responsável',
-            fixed: false
-        },
-        {
-            id: 'setorOrigem.nome',
-            label: 'Setor de Origem',
-            fixed: false
-        },
-        {
-            id: 'unidadeResponsavel.nome',
-            label: 'Unidade Responsável',
-            fixed: false
-        },
-        {
-            id: 'setorResponsavel.nome',
-            label: 'Setor Responsável',
-            fixed: false
-        },
-        {
-            id: 'usuarioConclusaoPrazo.nome',
-            label: 'Usuário da Conclusao do Prazo',
-            fixed: false
-        },
-        {
-            id: 'criadoPor.nome',
-            label: 'Criado Por',
-            fixed: false
-        },
-        {
-            id: 'criadoEm',
-            label: 'Criado Em',
-            fixed: false
-        },
-        {
-            id: 'atualizadoPor.nome',
-            label: 'Atualizado Por',
-            fixed: false
-        },
-        {
-            id: 'atualizadoEm',
-            label: 'Atualizado Em',
-            fixed: false
-        },
-        {
-            id: 'apagadoPor.nome',
-            label: 'Apagado Por',
-            fixed: false
-        },
-        {
-            id: 'apagadoEm',
-            label: 'Apagado Em',
-            fixed: false
-        },
-        {
-            id: 'actions',
-            label: '',
-            fixed: true
-        }
-    ];
-
-    columns = new FormControl();
 
     @Input()
     deletingIds: number[] = [];
@@ -236,18 +115,32 @@ export class CdkTarefaGridComponent implements AfterViewInit, OnInit, OnChanges 
     isIndeterminate = false;
     hasExcluded = false;
 
+    /*
+    * Drag and Drop
+    * */
+    @Input() parentIdentifier: string;
+    @Output() columnsDefinitionsChange: EventEmitter<TableColumn[]> = new EventEmitter<TableColumn[]>();
+
+    protected _resizing: boolean = false;
+    protected _tableColumns: TableColumn[] = []
+
     /**
      * @param _changeDetectorRef
      * @param _cdkSidebarService
      */
     constructor(
-        private _changeDetectorRef: ChangeDetectorRef,
-        private _cdkSidebarService: CdkSidebarService
+        protected _changeDetectorRef: ChangeDetectorRef,
+        protected _cdkSidebarService: CdkSidebarService,
+        protected _tableDefinitionsService: TableDefinitionsService,
     ) {
+        super(_tableDefinitionsService, _changeDetectorRef);
         this.gridFilter = {};
+        this._tableColumns = _.cloneDeep(CdkTarefaGridColumns.columns);
+        this._tableColumnsOriginal = _.cloneDeep(CdkTarefaGridColumns.columns);
     }
 
-    ngOnChanges(): void {
+    ngOnChanges(changes: SimpleChanges): void {
+        super.ngOnChanges(changes);
         this.dataSource = new TarefaDataSource(of(this.tarefas));
         this.paginator.length = this.total;
     }
@@ -266,23 +159,6 @@ export class CdkTarefaGridComponent implements AfterViewInit, OnInit, OnChanges 
         this.paginator.pageSize = this.pageSize;
 
         this.dataSource = new TarefaDataSource(of(this.tarefas));
-
-        this.columns.setValue(this.allColumns.map(c => c.id).filter(c => this.displayedColumns.indexOf(c) > -1));
-
-        this.columns.valueChanges.pipe(
-            debounceTime(300),
-            distinctUntilChanged(),
-            switchMap((values) => {
-                this.displayedColumns = [];
-                this.allColumns.forEach((c) => {
-                    if (c.fixed || (values.indexOf(c.id) > -1)) {
-                        this.displayedColumns.push(c.id);
-                    }
-                });
-                this._changeDetectorRef.markForCheck();
-                return of([]);
-            })
-        ).subscribe();
     }
 
     ngAfterViewInit(): void {
