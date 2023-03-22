75b51bef63a862fa4f07765cd9f10746d2dffd13
CT Anders <anders@marinha.mil.br>
2023-03-22 15:45:55 -0300

    modulo marinha setores

--- src/modules/modules-config.ts
+++ src/modules/modules-config.ts
@@ -1,3 +1,4 @@
+import { modulesConfigMarinha } from './marinha/app/modules-config'; //passo 2
 // import {modulesConfigJudicial} from './judicial/modules-config';
 //import {modulesConfigConsultivo} from './consultivo/modules-config';
 //import {modulesConfigDisciplinar} from './disciplinar/modules-config';
@@ -7,6 +8,7 @@
 // import {modulesConfigGestaoDevedor} from './gestao-devedor/modules-config';
 
 export const modulesConfig: any = [
+    modulesConfigMarinha, // passo 3
     // modulesConfigJudicial,
     //modulesConfigConsultivo,
     //modulesConfigDisciplinar,
